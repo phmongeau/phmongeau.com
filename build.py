7b844d267641d382290c223c3f9dee77752ac443
Philippe Mongeau <ph.mongeau@gmail.com>
2014-03-30 13:01:01 -0400

    add image captions support to the template script

--- build.py
+++ build.py
@@ -3,17 +3,21 @@
 import os
 from jinja2 import Environment
 from jinja2.loaders import FileSystemLoader
+import json
 
 loader = FileSystemLoader('templates')
 env = Environment(loader=loader)
 
 
-images = sorted(os.listdir("images/portfolio"), reverse=True)
-images = ["images/portfolio/{}".format(i) for i in images]
+img_paths = sorted(os.listdir("images/portfolio"), reverse=True)
+img_paths = [i for i in img_paths if not i.endswith(".json")]
 
 # TODO
-captions = [""] * len(images)
+with open("images/portfolio/captions.json", "r") as f:
+    captions = json.load(f)
+
+images = ((os.path.join("images/portfolio", i), captions.get(i, "")) for i in img_paths)
 
 t = env.get_template('index.html')
 with open("index.html", "w") as f:
-    f.write(t.render(images=zip(images, captions)))
+    f.write(t.render(images=images))
